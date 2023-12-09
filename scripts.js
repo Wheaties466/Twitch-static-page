4b7d07ed02fbc473af69152b094967006f52bddd
Wheaties466 <Mwheat42289@gmail.com>
2023-12-09 17:33:19 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,44 +1,46 @@
-// Define your streamers list
-const streamers = ['streamer1', 'streamer2', 'streamer3']; // and so on...
-
-// Function to create an embed link
-function createEmbedLink(streamer) {
-    // Replace with actual embed URL logic
-    return `https://service.com/embed/${streamer}`;
-}
-
-// Function to check if a stream is live
-async function isStreamerLive(streamer) {
-    // Replace with actual API call logic
-    // This should return true or false
+// Function to create an embed link (replace with actual Twitch embed logic)
+function createTwitchEmbed(streamer) {
+    // Example Twitch Embed initialization
+    new Twitch.Embed("twitch-embed-" + streamer, {
+        width: 854,
+        height: 480,
+        channel: streamer,
+        // Add other necessary parameters
+    });
 }
 
 // Function to render streams
-async function renderStreams() {
+function renderStreams(streamers) {
     const liveStreams = document.getElementById('live-streams');
     const offlineStreams = document.getElementById('offline-streams');
 
     liveStreams.innerHTML = '';
     offlineStreams.innerHTML = '';
 
-    for (const streamer of streamers) {
+    streamers.forEach(streamer => {
         const streamDiv = document.createElement('div');
+        streamDiv.id = 'twitch-embed-' + streamer;
         streamDiv.className = 'stream';
-        streamDiv.innerHTML = `<iframe src="${createEmbedLink(streamer)}"></iframe>`;
 
-        if (await isStreamerLive(streamer)) {
+        // Example embed - you need to replace with actual embed logic
+        createTwitchEmbed(streamer);
+
+        // Example logic - replace with actual live/offline check
+        if (/* Check if streamer is live */) {
             liveStreams.appendChild(streamDiv);
         } else {
             offlineStreams.appendChild(streamDiv);
         }
-    }
+    });
 }
 
-// Function to refresh streams every 5 minutes
-function refreshStreams() {
-    renderStreams();
-    setTimeout(refreshStreams, 5 * 60 * 1000); // 5 minutes
-}
+// Fetch streamers and render streams
+fetch('streamers.txt')
+   .then(response => response.text())
+   .then(text => {
+       const streamers = text.split('\n').filter(Boolean);
+       renderStreams(streamers);
+   })
+   .catch(error => console.error('Error fetching streamers list:', error));
 
-// Initial call to render streams
-refreshStreams();
+// Include logic to periodically check stream status (if needed)
