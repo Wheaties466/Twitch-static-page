5a5f9836090c11d137bc0ec03d7fba94ff325010
Wheaties466 <Mwheat42289@gmail.com>
2023-12-09 23:01:05 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,63 +1,102 @@
-// Function to hide a specific stream
+// Function to hide a specific stream and set a cookie
 function hideStream(streamDivId) {
     const streamDiv = document.getElementById(streamDivId);
     if (streamDiv) {
         streamDiv.style.display = 'none';
+        setHiddenStreamCookie(streamDivId);
     }
+}
+
+// Set cookie for hidden streams
+function setHiddenStreamCookie(streamDivId) {
+    document.cookie = `hidden_${streamDivId}=true; max-age=86400; path=/`;
+}
+
+// Function to show all streams in a grid layout
+function showAllStreamsInGrid() {
+    const streams = document.querySelectorAll('.stream');
+    streams.forEach(stream => {
+        stream.style.display = 'grid';
+    });
 }
 
 // Function to create a Twitch embed for a given streamer
 function createTwitchEmbed(streamer, container) {
     const embedDivId = 'twitch-embed-' + streamer;
     const streamDivId = 'stream-div-' + streamer;
-    
+
     const streamDiv = document.createElement('div');
     streamDiv.id = streamDivId;
     streamDiv.className = 'stream';
 
+    const streamHeader = document.createElement('h3');
+    streamHeader.innerText = streamer;
+
     const embedDiv = document.createElement('div');
     embedDiv.id = embedDivId;
+    embedDiv.className = 'twitch-embed';
 
-    // Hide button
     const hideButton = document.createElement('button');
     hideButton.innerText = 'Hide';
     hideButton.className = 'hide-button';
     hideButton.onclick = function() { hideStream(streamDivId); };
 
+    streamDiv.appendChild(streamHeader);
     streamDiv.appendChild(embedDiv);
     streamDiv.appendChild(hideButton);
 
     container.appendChild(streamDiv);
 
+    $(streamDiv).resizable({
+        minHeight: 300,
+        minWidth: 300,
+        resize: function(event, ui) {
+            $(this).find('.twitch-embed').each(function() {
+                $(this).width(ui.size.width);
+                $(this).height(ui.size.height - streamHeader.outerHeight(true) - hideButton.outerHeight(true));
+            });
+        }
+    }).draggable({
+        containment: 'body',
+        scroll: false
+    });
+
     new Twitch.Embed(embedDivId, {
-        width: 854,
-        height: 480,
+        width: '100%',
+        height: '100%',
         channel: streamer,
-        parent: ["wheaties466.github.io"] // Replace with your GitHub Pages URL
+        parent: ["wheaties466.github.io"]
     });
 }
 
-// Rest of your scripts.js file...
-
-
 // Function to render streams
-async function renderStreams(streamers) {
+function renderStreams(streamers) {
     const liveStreams = document.getElementById('live-streams');
     const offlineStreams = document.getElementById('offline-streams');
 
     liveStreams.innerHTML = '';
     offlineStreams.innerHTML = '';
 
-    for (const streamer of streamers) {
-        if (await checkIfLive(streamer)) { // Replace with actual logic to check if live
-            createTwitchEmbed(streamer, liveStreams);
-        } else {
-            createTwitchEmbed(streamer, offlineStreams);
-        }
-    }
+    streamers.forEach(streamer => {
+        createTwitchEmbed(streamer, liveStreams);
+    });
+
+    checkHiddenStreams();
 }
 
-// Fetch streamers from the text file and render streams
+// Check cookies on page load and hide streams if necessary
+function checkHiddenStreams() {
+    const cookies = document.cookie.split(';');
+    cookies.forEach(cookie => {
+        const [name, value] = cookie.trim().split('=');
+        if (name.startsWith('hidden_') && value === 'true') {
+            const streamDivId = name.substring(7);
+            hideStream(streamDivId);
+        }
+    });
+}
+
+// Fetch streamers from the text file, render streams, and set up periodic status check
 fetch('streamers.txt')
     .then(response => response.text())
     .then(text => {
@@ -67,18 +106,8 @@ fetch('streamers.txt')
     .catch(error => console.error('Error fetching streamers list:', error));
 
 // Event listeners for the toggle buttons
-document.getElementById('show-live').addEventListener('click', function() {
-    document.getElementById('live-streams').style.display = 'flex';
-    document.getElementById('offline-streams').style.display = 'none';
-});
-
-document.getElementById('show-offline').addEventListener('click', function() {
-    document.getElementById('live-streams').style.display = 'none';
-    document.getElementById('offline-streams').style.display = 'flex';
-});
-
-// Placeholder function - replace with actual Twitch API call
-async function checkIfLive(streamer) {
-    // Logic to check if the streamer is live
-    return true; // Replace with actual live check
-}
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('show-live').addEventListener('click', function() {
+        showAllStreamsInGrid();
+        document.getElementById('live-streams').style.display = 'grid';
+        document.getElementById('offline-streams').
